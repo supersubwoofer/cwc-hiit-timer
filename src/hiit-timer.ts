a2d696a9542f9e07a6e7bf842efcae360d443a6d
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-06-02 17:32:08 -0400

    add layout skeleton

--- src/hiit-timer.ts
+++ src/hiit-timer.ts
@@ -7,6 +7,7 @@ import { Model, ControlState, GetControlState } from './stateHelper';
 @customElement('hiit-timer')
 export class HiitTimer extends LitElement {
   @property({type : String}) title = "Sprint-8";
+  @property({type : String}) description = "Sprint-8 description ...";
   @property({type : Number}) set = 8;
   @property({type : Number}) warmUp = 300;
   @property({type : Number}) sprint = 20;
@@ -27,32 +28,15 @@ export class HiitTimer extends LitElement {
       isPause: this.isPause
     });
 
-    switch(controlState) {
-      case ControlState.Standby: return view.standby(this.title, this.StartHandler);
-      case ControlState.Warmup: 
-        return view.warmUp(
-        secToHMS(countDown(this.warmUp, this.counter), 'mm:ss'), 
-        secToHMS(this.counter), this.PauseHandler);
-      case ControlState.Sprint: return view.sprint(
-        currentRound(this.counter, this.warmUp, this.sprint, this.rest),
-        secToHMS(countDown(this.sprint, sprintCounter(this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
-        secToHMS(this.counter), this.PauseHandler);
-      case ControlState.Rest:
-        return view.rest(
-        currentRound(this.counter, this.warmUp, this.sprint, this.rest),
-        secToHMS(countDown(this.rest, restCounter(this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
-        secToHMS(this.counter), this.PauseHandler);
-      case ControlState.Cooldown:
-        return view.coolDown(
-        secToHMS(countDown(this.coolDown, coolDownCounter(this.set, this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
-        secToHMS(this.counter));
-      case ControlState.Done:
-        return view.done(secToHMS(this.counter));
-      default:
-        return view.error();
-    }
+    let buttonsTemplate = view.layoutButtons(
+      this.isPause, this.StartHandler, this.PauseHandler, this.ResetHandler);
+
+    return (controlState == ControlState.Standby || controlState == ControlState.Done)?
+      view.layoutMain(this.title, this.GetBodyTemplate(controlState)) :
+      view.layoutMain(this.title, this.GetBodyTemplate(controlState), buttonsTemplate); 
   }
 
+  // Lifecycle hooks
   updated(changedProperties) {
     let controlState = GetControlState({
       set: this.set,
@@ -70,6 +54,7 @@ export class HiitTimer extends LitElement {
 
   }
 
+  // Event Handlers & callbacks
   StartHandler() : void {
     clearInterval(this.timerID);
     this.timerID = setInterval(this.Increament, 1000, this);
@@ -86,4 +71,32 @@ export class HiitTimer extends LitElement {
   }
 
   Increament = (self) : void => self.counter = self.counter + 1;
+
+  // Helpers
+  GetBodyTemplate(controlState:ControlState){
+    switch(controlState) {
+      case ControlState.Standby: return view.standby(this.description, this.StartHandler);
+      case ControlState.Warmup: 
+        return view.warmUp(
+        secToHMS(countDown(this.warmUp, this.counter), 'mm:ss'), 
+        secToHMS(this.counter));
+      case ControlState.Sprint: return view.sprint(
+        currentRound(this.counter, this.warmUp, this.sprint, this.rest),
+        secToHMS(countDown(this.sprint, sprintCounter(this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
+        secToHMS(this.counter));
+      case ControlState.Rest:
+        return view.rest(
+        currentRound(this.counter, this.warmUp, this.sprint, this.rest),
+        secToHMS(countDown(this.rest, restCounter(this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
+        secToHMS(this.counter));
+      case ControlState.Cooldown:
+        return view.coolDown(
+        secToHMS(countDown(this.coolDown, coolDownCounter(this.set, this.counter, this.warmUp, this.sprint, this.rest)), 'mm:ss'), 
+        secToHMS(this.counter));
+      case ControlState.Done:
+        return view.done(secToHMS(this.counter), this.ResetHandler);
+      default:
+        return view.error();
+      }
+  }
 }
--- src/presentation.ts
+++ src/presentation.ts
@@ -1,41 +1,52 @@
-import { html } from 'lit-html';
+import { html, TemplateResult } from 'lit-html';
 
-export const error = () => html`<p>Error</p>`;
-export const standby = (title, startActionHandler) => html`
-  <p>${title}</p>
+export const layoutMain = (title:string, bodyTemplate:TemplateResult, buttonSectionTemplate?:TemplateResult) => html`
+  <p class="hiit-title">${title}</p>
+  ${bodyTemplate}
+  ${buttonSectionTemplate}
+  `;
+
+const layoutBody = (subTitle:string, countDown:string, timer:string, roundNumber?:number) => html`
+  <div class="hiit-body">
+    ${roundNumber?
+      html`<p>Round: ${roundNumber}</p>`:
+      html``
+    }
+    <p>${subTitle}</p>
+    <p>${countDown}</p>
+    <p>${timer}</p>
+  </div>
+  `;
+
+export const layoutButtons = (isPause:boolean, startActionHandler, pauseHandler, resetActionHandler) => html`
+  <div class="hiit-buttons">
+  <button @click="${startActionHandler}">Start</button>
+  <button @click="${pauseHandler}">Pause</button>
+  <button @click="${resetActionHandler}">Restart</button>
+  </div>
+  `;
+
+export const standby = (workoutDescription:string, startActionHandler) => html`
+  <p>${workoutDescription}</p>
   <button @click="${startActionHandler}">Start</button>
   `;
 
-export const warmUp = (countDown, timer, pauseHandler) => html`
-  <p>Warm up</p>
-  <p>${countDown}</p>
-  <p>${timer}</p>
-  <button @click="${pauseHandler}">Pause</button>
-  `;
+export const warmUp = (countDown:string, timer:string) => 
+  layoutBody("Warm Up", countDown, timer);
 
-export const sprint = (roundNumber, countDown, timer, pauseHandler) => html`
-  <p>Round: ${roundNumber}</p>
-  <p>Sprint</p>
-  <p>${countDown}</p>
-  <p>${timer}</p>
-  <button @click="${pauseHandler}">Pause</button>
-  `;
+export const sprint = (roundNumber:number, countDown:string, timer:string) => 
+  layoutBody("Sprint", countDown, timer, roundNumber);
 
-export const rest = (roundNumber, countDown, timer, pauseHandler) => html`
-  <p>Round: ${roundNumber}</p>
-  <p>Rest</p>
-  <p>${countDown}</p>
-  <p>${timer}</p>
-  <button @click="${pauseHandler}">Pause</button>
-  `;
+export const rest = (roundNumber:number, countDown:string, timer:string) => 
+  layoutBody("Rest", countDown, timer, roundNumber);
 
-export const coolDown = (countDown, timer) => html`
-  <p>Cool Down</p>
-  <p>${countDown}</p>
-  <p>${timer}</p>
-  `;
+export const coolDown = (countDown:string, timer:string) => 
+  layoutBody("Cool Down", countDown, timer);
 
-export const done = (timer) => html`
+export const done = (timer:string, resetActionHandler) => html`
   <p>Done !</p>
   <p>You have finished a HIIT session of ${timer}.</p>
+  <button @click="${resetActionHandler}">Restart</button>
   `;
+
+export const error = () => html`<p>Error</p>`;
